a6dd4b3a7351e71d118d8e4eaa91eb4d879600d5
LakshmiNarsitha <79990816+LakshmiNarsitha@users.noreply.github.com>
2022-02-28 20:08:57 +0530

    Create Poisson Distribution Week 7.r

---
+++ Poisson Distribution Week 7.r
@@ -0,0 +1,30 @@
+print("enter data : ")
+x <- scan()
+print("enter frequencies :")
+f <- scan()
+fx <- f*x
+sumfx <- sum(fx)
+sumf <- sum(f)
+s<- length(x)-1
+p <- sumfx/sumf
+pr <- dpois(0:s,lambda=p)
+pr <- round(pr,digits=5)
+fre <-  sumf*pr
+fre <- round(fre,digits=2)
+fe <- round(fre,digits=0)
+d <- data.frame(x,f,fx,pr,fre,fe)
+l <- c(NA,sumf,sumfx,NA,NA,sum(fe))
+mydata <- rbind(d,l)
+print(mydata)
+result <- chisq.test(f,p=pr,rescale.p=TRUE)
+print(result)
+calvalue <- result$statistic
+sprintf("caluculated value is : %f",calvalue)
+tablevalue <- qchisq(.95,df=s)
+sprintf("table value is : %f",tablevalue)
+if(calvalue<tablevalue) {
+  print("accept null hypothesis")
+} else {
+  print("reject null hypothesis")
+}
+
