778dc62255930ac5ef4e93c5eb260f0d90e2fa3e
LakshmiNarsitha <79990816+LakshmiNarsitha@users.noreply.github.com>
2022-02-28 19:50:03 +0530

    Create Binomial Distribution Lab Cycle.r

---
+++ Binomial Distribution Lab Cycle.r
@@ -0,0 +1,31 @@
+print("enter data : ")
+x <- scan()
+print("enter frequencies :")
+f <- scan()
+fx <- f*x
+sumfx <- sum(fx)
+sumf <- sum(f)
+s<- length(x)-1
+p <- sumfx/sumf/s
+pr <- dbinom(0:s,size=s,prob=p)
+pr <- round(pr,digits=5)
+fre <-  sumf*pr
+fre <- round(fre,digits=2)
+fe <- round(fre,digits=0)
+d <- data.frame(x,f,fx,pr,fre,fe)
+print(d)
+l <- c(NA,sumf,sumfx,NA,NA,sum(fe))
+mydata <- rbind(d,l)
+print(mydata)
+result <- chisq.test(f,p=pr,rescale.p=TRUE)
+print(result)
+calvalue <- result$statistic
+sprintf("caluculated value is : %f",calvalue)
+tablevalue <- qchisq(.95,df=s)
+print(tablevalue)
+if(calvalue<tablevalue) {
+  print("accept")
+} else {
+  print("reject")
+}
+
